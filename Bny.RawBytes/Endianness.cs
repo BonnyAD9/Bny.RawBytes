fa6935a417ff45bc0427ac16ec95ea0e7d69b0dd
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-08-24 19:38:00 +0200

    Change XML Documentation

--- Bny.RawBytes/Endianness.cs
+++ Bny.RawBytes/Endianness.cs
@@ -1,12 +1,13 @@
 ﻿namespace Bny.RawBytes;
 
 /// <summary>
-/// Byte order
+/// Byte order. <c>Endianness.Default</c> byte order is usually the fastest because it usually is just noop or copy.
 /// </summary>
 public enum Endianness
 {
     /// <summary>
-    /// The C# default (usually same little-endian)
+    /// The C# default (usually same as <c>Endianess.Little</c>). You can get the default endianness with <c>Bytes.IsDefaultLE</c> property.
+    /// This is usually faster than the other two.
     /// </summary>
     Default,
     /// <summary>
@@ -16,5 +17,5 @@ public enum Endianness
     /// <summary>
     /// Big-Endian byte order (most significant byte first)
     /// </summary>
-    Big
+    Big,
 }
