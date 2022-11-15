1018e230ba6ef4d2f493abe9adce6f59ac744a6e
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-11-15 21:30:14 +0100

    Add from functions

--- Bny.RawBytes.Tester/Program.cs
+++ Bny.RawBytes.Tester/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Bny.RawBytes;
 using System.Numerics;
 
-var arr = new byte[] { 255, 255, 255, 255 }.AsSpan();
-Console.WriteLine(Bytes.To<int>(arr, Endianness.Big)); // -1
+var arr = new byte[4];
 
-arr = new byte[] { 2, 0, 255 };
-Console.WriteLine(Bytes.To<ushort>(arr[..2], Endianness.Big)); // 512
+Bytes.From<ushort>(512, arr);
+
+foreach (var b in arr)
+    Console.WriteLine(b);
--- Bny.RawBytes/Bytes.cs
+++ Bny.RawBytes/Bytes.cs
@@ -31,8 +31,8 @@ public static class Bytes
     /// <param name="signed"></param>
     /// <param name="index"></param>
     /// <returns></returns>
-    public static T To<T>(ReadOnlySpan<byte> data, Endianness endianness = Endianness.Default, bool? signed = null, int index = 0) where T : new()
-        => (T)To(data, typeof(T), endianness, signed, index);
+    public static T To<T>(ReadOnlySpan<byte> data, Endianness endianness = Endianness.Default, bool? signed = null) where T : new()
+        => (T)To(data, typeof(T), endianness, signed);
 
     /// <summary>
     /// Converts byte array to the given type
@@ -44,22 +44,22 @@ public static class Bytes
     /// <param name="index"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentException"></exception>
-    public static object To(ReadOnlySpan<byte> data, Type type, Endianness endianness = Endianness.Default, bool? signed = null, int index = 0)
+    public static object To(ReadOnlySpan<byte> data, Type type, Endianness endianness = Endianness.Default, bool? signed = null)
     {
         object res = Activator.CreateInstance(type)!;
-        if (TryReadIBInaryInteger(data, ref res, type, index, endianness, signed))
+        if (TryReadIBinaryInteger(data, ref res, type, endianness, signed))
             return res;
         throw new ArgumentException("Cannot convert to this value type", nameof(type));
     }
 
-    private static bool TryReadIBInaryInteger(ReadOnlySpan<byte> data, ref object result, Type type, int index = 0, Endianness endianness = Endianness.Default, bool? signed = false)
+    private static bool TryReadIBinaryInteger(ReadOnlySpan<byte> data, ref object result, Type type, Endianness endianness = Endianness.Default, bool? signed = false)
     {
         string mname = endianness switch
         {
             Endianness.Big => "ReadBigEndian",
             Endianness.Little => "ReadLittleEndian",
             Endianness.Default => IsDefaultLE ? "ReadLittleEndian" : "ReadBigEndian",
-            _ => throw new ArgumentException("Invalid endianness value")
+            _ => throw new ArgumentException("Invalid endianness value", nameof(endianness)),
         };
 
         var interfaces = type.GetInterfaces();
@@ -72,12 +72,73 @@ public static class Bytes
         try
         {
             var arr = data.ToArray();
-            result = bi.GetMethod(mname, new Type[] { typeof(byte[]), typeof(int), typeof(bool) })!.Invoke(result, new object[] { arr, index, isUnsigned })!;
+            result = bi.GetMethod(mname, new Type[] { typeof(byte[]), typeof(int), typeof(bool) })!.Invoke(result, new object[] { arr, 0, isUnsigned })!;
         }
         catch
         {
             return false;
         }
         return true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <param name="result"></param>
+    /// <param name="endianness"></param>
+    /// <returns></returns>
+    public static int From<T>(T value, Span<byte> result, Endianness endianness = Endianness.Default)
+        => From(value!, result, typeof(T), endianness);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="value"></param>
+    /// <param name="type"></param>
+    /// <param name="endianness"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int From(object value, Span<byte> result, Type type, Endianness endianness = Endianness.Default)
+    {
+        int len = TryWriteIBinaryInteger(value, result, type, endianness);
+        if (len != 0)
+            return len;
+        throw new ArgumentException("Cannot convert from this value type", nameof(value));
+    }
+
+    private static int TryWriteIBinaryInteger(object data, Span<byte> result, Type type, Endianness endianness = Endianness.Default)
+    {
+        string mname = endianness switch
+        {
+            Endianness.Big => "WriteBigEndian",
+            Endianness.Little => "WriteLittleEndian",
+            Endianness.Default => IsDefaultLE ? "WriteLittleEndian" : "WriteBigEndian",
+            _ => throw new ArgumentException("Invalid endianness value", nameof(endianness)),
+        };
+
+        var interfaces = type.GetInterfaces();
+        var bi = interfaces.FirstOrDefault(p => p.FullName is not null && p.FullName.Contains("System.Numerics.IBinaryInteger"));
+        if (bi is null)
+            return 0;
+
+        try
+        {
+            int byteCount = (int)bi.GetMethod("GetByteCount", Array.Empty<Type>())!.Invoke(data, Array.Empty<object>())!;
+            if (result.Length < byteCount)
+                return 0;
+
+            byte[] arr = new byte[byteCount];
+            int r = (int)bi.GetMethod(mname, new Type[] { typeof(byte[]) })!.Invoke(data, new object[] { arr })!;
+
+            arr.AsSpan()[..r].CopyTo(result);
+            return r;
+        }
+        catch
+        {
+            return 0;
+        }
     }
 }
