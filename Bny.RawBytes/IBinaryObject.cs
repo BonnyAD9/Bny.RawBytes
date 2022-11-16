03a1bd235a21d982a747a2226548e47671076348
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-11-16 16:47:51 +0100

    Add IBinaryObject interfaces

---
+++ Bny.RawBytes/IBinaryObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bny.RawBytes;
+
+/// <summary>
+/// Methods for converting the type to and from binary data
+/// </summary>
+public interface IBinaryObject
+{
+    /// <summary>
+    /// Tries to read the object from binary data
+    /// </summary>
+    /// <param name="data">binary data to read from</param>
+    /// <param name="result">the resulting object, shouldn't be null of this returns true</param>
+    /// <param name="endianness">prefered endianness of the conversion</param>
+    /// <returns>True on success, otherwise false</returns>
+    public static abstract bool TryFromBinary(ReadOnlySpan<byte> data, [NotNullWhen(true)] out object? result, Endianness endianness = Endianness.Default);
+
+    /// <summary>
+    /// Tries to convert the object to binary data
+    /// </summary>
+    /// <param name="result">result binary data</param>
+    /// <param name="endianness">prefered endianness of the conversion or default</param>
+    /// <returns>Number of bytes written to result, negative on error</returns>
+    public int TryToBinary(Span<byte> result, Endianness endianness = Endianness.Default);
+}
---
+++ Bny.RawBytes/IBinaryObjectT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bny.RawBytes;
+
+/// <summary>
+/// Generic methods for converting the type to and from binary data
+/// </summary>
+public interface IBinaryObject<TSelf> : IBinaryObject where TSelf : IBinaryObject<TSelf>
+{
+    /// <summary>
+    /// Tries to read the object from binary data
+    /// </summary>
+    /// <typeparam name="T">Type of the data</typeparam>
+    /// <param name="data">binary data to read from</param>
+    /// <param name="result">the resulting object, shouldn't be null of this returns true</param>
+    /// <param name="endianness">prefered endianness of the conversion</param>
+    /// <returns>True on success, otherwise false</returns>
+    public static abstract bool TryFromBinary(ReadOnlySpan<byte> data, [NotNullWhen(true)] out TSelf? result, Endianness endianness = Endianness.Default);
+
+    static bool IBinaryObject.TryFromBinary(ReadOnlySpan<byte> data, [NotNullWhen(true)] out object? result, Endianness endianness)
+    {
+        var ret = TSelf.TryFromBinary(data, out TSelf? res, endianness);
+        result = res;
+        return ret;
+    }
+}
